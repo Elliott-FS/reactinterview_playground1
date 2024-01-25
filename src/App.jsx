36b61d9e61664db27c96915d1287c23e4c3c4b91
Elliott Delano Wynn III <delanowynn@yahoo.com>
2024-01-25 18:13:03 -0500

    reset run

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,12 @@
-import { useState, useEffect } from 'react';
-import "./styles.css";
-import axios from 'axios';
+import { useState} from 'react';
 
-// https://randomuserapi.me/api
-
-// axios.get('/user?ID=12345')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
-
-async function getRandomUsers() {
-  try {
-    const data = await axios.get("/api/users/random_user?size=3");
-    console.log(data);
-    return JSON.stringify(data);
-  } catch(err) {
-    console.log("error: ", err);
-  }
-}
 
 
 function App() {
-  const [counter, setCounter] = useState(0); 
-  const [randomDataJson, setRandomDataJson] = useState("");
-
-  useEffect(() => {
-    getRandomUsers().then((randomData) => {
-      setRandomDataJson(randomData);
-    })
-  },[])
  
-
   return (
     <>
-      <div className = "App">
-        <div className='container'>
-        <h1>Hello coding sandbox!</h1>
-        <h2>Start coding to see some magic happen!</h2>
-        <h1>
-          {counter}
-        </h1>
-        <button onClick={() => {
-          setCounter(counter + 1);
-        }}>Increment +</button>
-        <button onClick={() => {
-          setCounter(counter - 1);
-        }}>Decrement -</button>
-        <p>
-          {randomDataJson}
-        </p>
-        </div>
-      </div>
+     <h1>Hello World</h1>
     </>
   )
 }
