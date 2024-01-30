ac9f20ca0ea0c212718c8220f0d72f367dcf1130
Elliott Delano Wynn III <delanowynn@yahoo.com>
2024-01-30 00:35:27 -0500

    new run/playing with fetch syntax

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,42 @@ import axios from "axios";
 //     // always executed
 //   });
 
-
+const fetchRandomData = () => {
+ return axios.get('https://randomuser.me/api')
+.then(({data}) => {
+    // handle success
+    console.log(data);
+    return JSON.stringify(data)
+  })
+.catch((error) => {
+  // handle error
+  console.log(error);
+})
+}
 
 
 function App() {
-  return (
-    <h1>Hello World</h1>
+
+const [counter, setCounter] = useState(0);
+const [userData, setUserData] = useState("Random Data");
+
+useEffect(() => {
+  fetchRandomData().then((randomData) => {
+    setUserData(randomData);
+  })
+},[])
+
+return (
+  <div className="container">
+    <h2>{counter}</h2>
+    <button onClick={() => {
+      setCounter(counter - 1)
+    }}>Decrement -</button>
+    <button onClick={() => {
+      setCounter(counter + 1)
+    }}>Increment +</button>
+    <p>{userData}</p>
+  </div>
   )
 }
 
